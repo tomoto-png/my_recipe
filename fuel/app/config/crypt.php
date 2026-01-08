700409d14a623c44c49cff7e422c99e3403c649c
tomoto-png <hirosi.tomoto0130@gmail.com>
2026-01-08 22:39:42 +0900

    環境構築

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+  'crypto_key' => 'As4xAkv_4x2QcSklXMfnAYjo',
+  'crypto_iv' => '_w4J1YgqAJVAuaoENY-W02Y0',
+  'crypto_hmac' => 'BnkiM4C_UguILHweIIfxI9yw',
+);
---
+++ fuel/app/logs/2026/01/08.php
@@ -0,0 +1,12 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2026-01-08 21:43:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-01-08 21:46:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-01-08 21:47:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-01-08 21:48:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-01-08 21:49:39 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-01-08 22:09:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2026-01-08 22:09:36 --> Warning - mysqli::__construct(): (HY000/1049): Unknown database 'fuelphp' in /var/www/html/app/fuel/core/classes/database/mysqli/connection.php on line 131
+WARNING - 2026-01-08 22:11:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2026-01-08 22:11:11 --> Warning - mysqli::__construct(): (HY000/1049): Unknown database 'fuelphp' in /var/www/html/app/fuel/core/classes/database/mysqli/connection.php on line 131
+WARNING - 2026-01-08 22:16:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
