0343a599e69bdca616aac21cf0544a530706862a
tomoto-png <hirosi.tomoto0130@gmail.com>
2026-01-21 12:37:37 +0900

    authフロント周りの修正 (#5)

--- fuel/app/views/auth/login.php
+++ fuel/app/views/auth/login.php
@@ -5,44 +5,55 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/assets/css/auth.css">
+	<link rel="stylesheet" href="/assets/css/base.css">
+	<link rel="stylesheet" href="/assets/css/utilities.css">
 	<title>ログイン</title>
 </head>
 
-<body>
-	<div class="auth-form">
-		<h1 class="mb24 text-center">ログイン</h1>
+<body class="flex items-center justify-center bg-white">
+	<div class="auth-form w-full p-32">
+		<h1 class="mb-24 text-center">ログイン</h1>
 
 		<?php if ($msg = Session::get_flash('error')): ?>
 			<p class="error">
-				<?php echo e($msg); ?>
+				<?= e($msg); ?>
 			</p>
 		<?php endif; ?>
 
-		<?php echo Form::open(['action' => 'auth/login']); ?>
-		<?php echo Form::csrf(); ?>
+		<?= Form::open(['action' => 'auth/login']); ?>
+		<?= Form::csrf(); ?>
 
-		<p class="form-row">
-			<?php echo Form::label('メールアドレス', 'email'); ?>
-			<?php echo Form::input('email', Input::post('email')); ?>
-		</p>
+		<div class="auth-form__group flex-col mb-16">
+			<div class="auth-form__row flex items-center">
+				<?= Form::label('メールアドレス', 'email', ['class' => 'auth-form__label']); ?>
+				<?= Form::input('email', Input::post('email'), ['class' => 'auth-form__input p-6']); ?>
+			</div>
 
-		<?php if (!empty($errors['email'])): ?>
-			<div class="error mb16"><?= e($errors['email']); ?></div>
-		<?php endif; ?>
+			<?php if (!empty($errors['email'])): ?>
+				<p class="auth-form__error error">
+					<?= e($errors['email']); ?>
+				</p>
+			<?php endif; ?>
+		</div>
 
-		<p class="form-row">
-			<?php echo Form::label('パスワード', 'password'); ?>
-			<?php echo Form::password('password'); ?>
-		</p>
+		<div class="auth-form__group flex-col">
+			<div class="auth-form__row flex items-center">
+				<?= Form::label('パスワード', 'password', ['class' => 'auth-form__label']); ?>
+				<?= Form::password('password', null, ['class' => 'auth-form__input p-6']); ?>
+			</div>
 
-		<?php if (!empty($errors['password'])): ?>
-			<div class="error mb16"><?= e($errors['password']); ?></div>
-		<?php endif; ?>
+			<?php if (!empty($errors['password'])): ?>
+				<p class="auth-form__error error">
+					<?= e($errors['password']); ?>
+				</p>
+			<?php endif; ?>
+		</div>
 
-		<?php echo Form::submit('submit', 'ログイン', ['class' => 'btn btn-primary w100 mt24']); ?>
-		<?php echo Form::close(); ?>
-		<p class="mt16 text-center">
-			<a href="<?php echo Uri::create('auth/register'); ?>">アカウントをお持ちでない方はこちら</a>
+
+		<?= Form::submit('submit', 'ログイン', ['class' => 'btn btn-add w-full mt-24']); ?>
+		<?= Form::close(); ?>
+		<p class="mt-16 text-center">
+			<a href="<?= Uri::create('auth/register'); ?>">アカウントをお持ちでない方はこちら</a>
 		</p>
 	</div>
 </body>
