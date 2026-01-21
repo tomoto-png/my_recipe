d8e1ae9aea076227dcce151af37a9e9245fa3fb3
tomoto-png <hirosi.tomoto0130@gmail.com>
2026-01-21 12:36:36 +0900

    authフロント周りの修正

--- fuel/app/views/auth/register.php
+++ fuel/app/views/auth/register.php
@@ -5,53 +5,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/auth.css">
+    <link rel="stylesheet" href="/assets/css/base.css">
+    <link rel="stylesheet" href="/assets/css/utilities.css">
     <title>新規登録</title>
 </head>
 
-<body>
-    <div class="auth-form">
+<body class="flex items-center justify-center">
+    <div class="auth-form w-full p-32">
         <h1 class="mb-24 text-center">新規登録</h1>
 
         <?php if ($msg = Session::get_flash('error')): ?>
             <p class="error">
-                <?php echo e($msg); ?>
+                <?= e($msg); ?>
             </p>
         <?php endif; ?>
 
-        <?php echo Form::open(['action' => 'auth/register']); ?>
-        <?php echo Form::csrf(); ?>
+        <?= Form::open(['action' => 'auth/register']); ?>
+        <?= Form::csrf(); ?>
 
-        <p class="form-row">
-            <?php echo Form::label('メールアドレス', 'email'); ?>
-            <?php echo Form::input('email', Input::post('email')); ?>
-        </p>
+        <div class="auth-form__group flex-col mb-16">
+            <div class="auth-form__row flex items-center">
+                <?= Form::label('メールアドレス', 'email', ['class' => 'auth-form__label']); ?>
+                <?= Form::input('email', Input::post('email'), ['class' => 'auth-form__input p-6']); ?>
+            </div>
 
-        <?php if (!empty($errors['email'])): ?>
-            <div class="error mb16"><?php echo $errors['email']; ?></div>
-        <?php endif; ?>
+            <?php if (!empty($errors['email'])): ?>
+                <p class="auth-form__error error">
+                    <?= e($errors['email']); ?>
+                </p>
+            <?php endif; ?>
+        </div>
 
-        <p class="form-row">
-            <?php echo Form::label('パスワード', 'password'); ?>
-            <?php echo Form::password('password'); ?>
-        </p>
+        <div class="auth-form__group flex-col mb-16">
+            <div class="auth-form__row flex items-center">
+                <?= Form::label('パスワード', 'password', ['class' => 'auth-form__label']); ?>
+                <?= Form::password('password', null, ['class' => 'auth-form__input p-6']); ?>
+            </div>
 
-        <?php if (!empty($errors['password'])): ?>
-            <div class="error mb16"><?php echo $errors['password']; ?></div>
-        <?php endif; ?>
+            <?php if (!empty($errors['password'])): ?>
+                <p class="auth-form__error error">
+                    <?= e($errors['password']); ?>
+                </p>
+            <?php endif; ?>
+        </div>
 
-        <p class="form-row">
-            <?php echo Form::label('パスワード(確認)', 'password_confirm'); ?>
-            <?php echo Form::password('password_confirm'); ?>
-        </p>
+        <div class="auth-form__group flex-col mb-16">
+            <div class="auth-form__row flex items-center">
+                <?= Form::label('パスワード(確認)', 'password_confirm', ['class' => 'auth-form__label']); ?>
+                <?= Form::password('password_confirm', null, ['class' => 'auth-form__input p-6']); ?>
+            </div>
 
-        <?php if (!empty($errors['password_confirm'])): ?>
-            <div class="error mb16"><?php echo $errors['password_confirm']; ?></div>
-        <?php endif; ?>
+            <?php if (!empty($errors['password_confirm'])): ?>
+                <p class="auth-form__error error">
+                    <?= e($errors['password_confirm']); ?>
+                </p>
+            <?php endif; ?>
+        </div>
 
-        <?php echo Form::submit('submit', '登録', ['class' => 'btn btn-primary w100 mt24']); ?>
-        <?php echo Form::close(); ?>
-        <p class="mt16 text-center">
-            <a href="<?php echo Uri::create('auth/login'); ?>">すでにアカウントをお持ちの方はこちら</a>
+        <?= Form::submit('submit', '登録', ['class' => 'btn btn-add w-full mt-24']); ?>
+        <?= Form::close(); ?>
+        <p class="mt-16 text-center">
+            <a href="<?= Uri::create('auth/login'); ?>">すでにアカウントをお持ちの方はこちら</a>
         </p>
     </div>
 </body>
