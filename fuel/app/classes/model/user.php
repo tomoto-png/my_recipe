244abdd844ab3f7747331420bc8300acb2e63f26
tomoto-png <hirosi.tomoto0130@gmail.com>
2026-01-27 20:30:57 +0900

    存在チェック時の取得項目をidのみに修正

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -2,16 +2,16 @@
 
 class Model_User
 {
-    protected static $table = 'users';
+    protected static $_table_name = 'users';
 
-    public static function exists_by_email($email)
+    public static function exists_by_email($email): bool
     {
-        $count = DB::select()
-            ->from(static::$table)
+        $table = static::$_table_name;
+        return (bool) \DB::select('id')
+            ->from($table)
             ->where('email', '=', $email)
+            ->limit(1)
             ->execute()
-            ->count();
-
-        return $count > 0;
+            ->current();
     }
 }
--- fuel/app/classes/validation/user.php
+++ fuel/app/classes/validation/user.php
@@ -5,7 +5,6 @@ class Validation_User
     //ユニークバリデーションの作成
     public static function _validation_unique_email($value)
     {
-        \Log::debug('unique_email called: ' . $value);
         if ($value === null || $value === '') {
             return true;
         }
